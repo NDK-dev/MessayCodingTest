2fa0872813e6654db9bf7d66d4b1c19b008a8aa3
Kazuhiro ISHIBASHI <k.ishibashi@ndk-group.co.jp>
2026-02-13 09:32:59 +0900

    update imports

--- composeApp/src/commonMain/kotlin/jp/co/ndk_group/messay_coding_test/App.kt
+++ composeApp/src/commonMain/kotlin/jp/co/ndk_group/messay_coding_test/App.kt
@@ -11,16 +11,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.painterResource
-
 import messaycodingtest.composeapp.generated.resources.Res
-import messaycodingtest.composeapp.generated.resources.compose_multiplatform
 import messaycodingtest.composeapp.generated.resources.messay_sdk
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 @Preview
